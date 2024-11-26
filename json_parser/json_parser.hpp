619b62da2fe42f71da47d7c73dfdc15846fe40dc
Hayk Khachatryan <hayk.g.khachatryan@gmail.com>
2024-11-26 14:57:16 +0400

    Add support for std::cout and remove the UIntType

--- json_parser/json_parser.hpp
+++ json_parser/json_parser.hpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 #include <variant>
 #include <cstdint>
 #include <vector>
@@ -7,22 +8,28 @@
 #include <algorithm>
 
 namespace hayk10002
-{    
+{   
+    //helper struct to overload lambda functions 
+    template<class... Ts>
+    struct overloaded : Ts... { using Ts::operator()...; };
+
+
     class Json
     {
     public:
-        using NullType = std::monostate;
-        using BoolType = bool;
-        using IntType = int64_t;
-        using UIntType = uint64_t;
-        using FloatType = double;
-        using StringType = std::string;
+        using NullType      = std::monostate;
+        using BoolType      = bool;
+        using IntType       = int64_t;
+        using FloatType     = double;
+        using StringType    = std::string;
+
         template<typename T = Json>
-        using ArrayTypeT = std::vector<T>;
-        using ArrayType = ArrayTypeT<>;
+        using ArrayTypeT    = std::vector<T>;
+        using ArrayType     = ArrayTypeT<>;
+
         template<typename T = Json>
-        using ObjectTypeT = std::unordered_map<StringType, T>; 
-        using ObjectType = ObjectTypeT<>;
+        using ObjectTypeT   = std::unordered_map<StringType, T>; 
+        using ObjectType    = ObjectTypeT<>;
 
     private:
         
@@ -30,7 +37,6 @@ namespace hayk10002
             NullType,
             BoolType,
             IntType,
-            UIntType,
             FloatType,
             StringType,
             ArrayType,
@@ -39,18 +45,42 @@ namespace hayk10002
 
     public:
 
-        static Json null() { return Json{}; }
-        static Json boolean(const BoolType& val) { Json res; res.m_data = val; return res; }
-        static Json number(const IntType& val) { Json res; res.m_data = val; return res; }
-        static Json number(const UIntType& val) { Json res; res.m_data = val; return res; }
-        static Json number(const FloatType& val) { Json res; res.m_data = val; return res; }
-        static Json string(const StringType& val) { Json res; res.m_data = val; return res; }
-        static Json array(const ArrayType& val) { Json res; res.m_data = val; return res; }
-        static Json object(const ObjectType& val) { Json res; res.m_data = val; return res; }
+        static Json null        ()                       { return Json{}; }
+        static Json boolean     (const BoolType&    val) { Json res; res.m_data = val; return res; }
+        static Json number_int  (const IntType&     val) { Json res; res.m_data = val; return res; }
+        static Json number_float(const FloatType&   val) { Json res; res.m_data = val; return res; }
+        static Json string      (const StringType&  val) { Json res; res.m_data = val; return res; }
+        static Json array       (const ArrayType&   val) { Json res; res.m_data = val; return res; }
+        static Json object      (const ObjectType&  val) { Json res; res.m_data = val; return res; }
 
         friend std::ostream& operator<<(std::ostream& out, const Json& val)
         {
-            
+            std::visit(overloaded{
+                [&out](const NullType&      arg) { std::cout << "null"; },
+                [&out](const BoolType&      arg) { out << (arg ? "true" : "false"); },
+                [&out](const IntType&       arg) { out << arg; },
+                [&out](const FloatType&     arg) { out << arg; },
+                [&out](const StringType&    arg) { out << std::quoted(arg); },
+                [&out](const ArrayType&     arg) 
+                { 
+                    out << '[';
+                    for (int i = 0; i < arg.size(); i++) out << (i != 0 ? ", " : "") << arg[i];
+                    out << ']';
+                },
+                [&out](const ObjectType&    arg) 
+                {
+                    out << '{';
+                    bool first = true;
+                    for (const auto& [key, value] : arg) 
+                    {
+                        if (!first) out << ", ";
+                        out << std::quoted(key) << ": " << value;
+                        first = false;
+                    }
+                    out << '}';
+                }
+            }, val.m_data);
+            return out;
         }
     };
 }
--- tests/main.cpp
+++ tests/main.cpp
@@ -6,13 +6,129 @@ using namespace hayk10002;
 
 int main()
 {
-    Json value = Json::object(
+    // test value taken from https://github.com/briandfoy/json-acceptance-tests/blob/master/json-checker/pass1.json 
+    Json value = Json::array(
+    {
+        Json::string("JSON Test Pattern pass1"),
+        Json::object({{"object with 1 member", Json::array({Json::string("array with 1 element")})}}),
+        Json::object({}),
+        Json::array({}),
+        Json::number_int(-42),
+        Json::boolean(true),
+        Json::boolean(false),
+        Json::null(),
+        Json::object(
         {
-            {"name", Json::boolean(true)}, 
-            {"age", Json::string("hehe")}
-        });
+            {"integer", Json::number_int(1234567890)},
+            {"real", Json::number_float(-9876.543210)},
+            {"e", Json::number_float(0.123456789e-12)},
+            {"E", Json::number_float(1.234567890E+34)},
+            {"", Json::number_float(23456789012E66)},
+            {"zero", Json::number_int(0)},
+            {"one", Json::number_int(1)},
+            {"space", Json::string(" ")},
+            {"quote", Json::string("")},
+            {"backslash", Json::string("\\")},
+            {"controls", Json::string("\b\f\n\r\t")},
+            {"slash", Json::string("/ & /")},
+            {"alpha", Json::string("abcdefghijklmnopqrstuvwyz")},
+            {"ALPHA", Json::string("ABCDEFGHIJKLMNOPQRSTUVWYZ")},
+            {"digit", Json::string("0123456789")},
+            {"0123456789", Json::string("digit")},
+            {"special", Json::string("`1~!@#$%^&*()_+-={':[,]}|;.</>?")},
+            {"hex", Json::string("\u0123\u4567\u89AB\uCDEF\uabcd\uef4A")},
+            {"true", Json::boolean(true)},
+            {"false", Json::boolean(false)},
+            {"null", Json::null()},
+            {"array", Json::array({})},
+            {"object", Json::object({})},
+            {"address", Json::string("50 St. James Street")},
+            {"url", Json::string("http://www.JSON.org/")},
+            {"comment", Json::string("// /* <!-- --")},
+            {"# -- --> */", Json::string(" ")},
+            {" s p a c e d ", Json::array({Json::number_int(1), Json::number_int(2), Json::number_int(3), Json::number_int(4), Json::number_int(5), Json::number_int(6), Json::number_int(7)})},
+            {"compact", Json::array({Json::number_int(1), Json::number_int(2), Json::number_int(3), Json::number_int(4), Json::number_int(5), Json::number_int(6), Json::number_int(7)})},
+            {"jsontext", Json::string("{\"object with 1 member\":[\"array with 1 element\"]}")},
+            {"quotes", Json::string("&#34; \u0022 %22 0x22 034 &#x22;")},
+            {"/\\\"\uCAFE\uBABE\uAB98\uFCDE\ubcda\uef4A\b\f\n\r\t`1~!@#$%^&*()_+-=[]{}|;:',./<>?", Json::string("A key can be any string")}            
+        }),
+        Json::number_float(0.5),
+        Json::number_float(98.6),
+        Json::number_float(99.44),
+        Json::number_int(1066),
+        Json::number_float(1e1),
+        Json::number_float(0.1e1),
+        Json::number_float(1e-1),
+        Json::number_float(1e00),
+        Json::number_float(2e+00),
+        Json::number_float(2e-00),
+        Json::string("rosebud")
+    });
 
-    //auto x = { 1, 4.5f, "343"};
+    std::cout << value;
 
     return 0;
 }
+
+/*
+this test value taken from https://github.com/briandfoy/json-acceptance-tests/blob/master/json-checker/pass1.json 
+[
+    "JSON Test Pattern pass1",
+    {"object with 1 member":["array with 1 element"]},
+    {},
+    [],
+    -42,
+    true,
+    false,
+    null,
+    {
+        "integer": 1234567890,
+        "real": -9876.543210,
+        "e": 0.123456789e-12,
+        "E": 1.234567890E+34,
+        "":  23456789012E66,
+        "zero": 0,
+        "one": 1,
+        "space": " ",
+        "quote": "\"",
+        "backslash": "\\",
+        "controls": "\b\f\n\r\t",
+        "slash": "/ & \/",
+        "alpha": "abcdefghijklmnopqrstuvwyz",
+        "ALPHA": "ABCDEFGHIJKLMNOPQRSTUVWYZ",
+        "digit": "0123456789",
+        "0123456789": "digit",
+        "special": "`1~!@#$%^&*()_+-={':[,]}|;.</>?",
+        "hex": "\u0123\u4567\u89AB\uCDEF\uabcd\uef4A",
+        "true": true,
+        "false": false,
+        "null": null,
+        "array":[  ],
+        "object":{  },
+        "address": "50 St. James Street",
+        "url": "http://www.JSON.org/",
+        "comment": "// /* <!-- --",
+        "# -- --> * /": " ",
+        " s p a c e d " :[1,2 , 3
+
+,
+
+4 , 5        ,          6           ,7        ],"compact":[1,2,3,4,5,6,7],
+        "jsontext": "{\"object with 1 member\":[\"array with 1 element\"]}",
+        "quotes": "&#34; \u0022 %22 0x22 034 &#x22;",
+        "\/\\\"\uCAFE\uBABE\uAB98\uFCDE\ubcda\uef4A\b\f\n\r\t`1~!@#$%^&*()_+-=[]{}|;:',./<>?"
+: "A key can be any string"
+    },
+    0.5 ,98.6
+,
+99.44
+,
+
+1066,
+1e1,
+0.1e1,
+1e-1,
+1e00,2e+00,2e-00
+,"rosebud"]
+
+*/
