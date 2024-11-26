6a56e06f8faec6fca09374d0e3fb85b8f0fcce4b
Hayk Khachatryan <hayk.g.khachatryan@gmail.com>
2024-11-26 11:49:15 +0400

    Change from constructor to static functions
    Also type names are now in the class

--- json_parser/json_parser.hpp
+++ json_parser/json_parser.hpp
@@ -3,22 +3,27 @@
 #include <cstdint>
 #include <vector>
 #include <unordered_map>
+#include <string>
+#include <algorithm>
 
 namespace hayk10002
-{
-    class Json;
-
-    typedef std::monostate NullType;
-    typedef bool BoolType;
-    typedef int64_t IntType;
-    typedef uint64_t UIntType;
-    typedef double FloatType;
-    typedef std::string StringType;
-    typedef std::vector<Json> ArrayType;
-    typedef std::unordered_map<StringType, Json> ObjectType;
-
+{    
     class Json
     {
+    public:
+        using NullType = std::monostate;
+        using BoolType = bool;
+        using IntType = int64_t;
+        using UIntType = uint64_t;
+        using FloatType = double;
+        using StringType = std::string;
+        template<typename T = Json>
+        using ArrayTypeT = std::vector<T>;
+        using ArrayType = ArrayTypeT<>;
+        template<typename T = Json>
+        using ObjectTypeT = std::unordered_map<StringType, T>; 
+        using ObjectType = ObjectTypeT<>;
+
     private:
         
         std::variant<
@@ -33,13 +38,19 @@ namespace hayk10002
         > m_data;
 
     public:
-        Json() = default;
-        Json(BoolType val): m_data(val) {}
-        Json(IntType val): m_data(val) {}
-        Json(UIntType val): m_data(val) {}
-        Json(FloatType val): m_data(val) {}
-        Json(StringType val): m_data(val) {}
-        Json(ArrayType val): m_data(val) {}
-        Json(ObjectType val): m_data(val) {}
+
+        static Json null() { return Json{}; }
+        static Json boolean(const BoolType& val) { Json res; res.m_data = val; return res; }
+        static Json number(const IntType& val) { Json res; res.m_data = val; return res; }
+        static Json number(const UIntType& val) { Json res; res.m_data = val; return res; }
+        static Json number(const FloatType& val) { Json res; res.m_data = val; return res; }
+        static Json string(const StringType& val) { Json res; res.m_data = val; return res; }
+        static Json array(const ArrayType& val) { Json res; res.m_data = val; return res; }
+        static Json object(const ObjectType& val) { Json res; res.m_data = val; return res; }
+
+        friend std::ostream& operator<<(std::ostream& out, const Json& val)
+        {
+            
+        }
     };
 }
--- tests/main.cpp
+++ tests/main.cpp
@@ -6,6 +6,13 @@ using namespace hayk10002;
 
 int main()
 {
-    Json value;
+    Json value = Json::object(
+        {
+            {"name", Json::boolean(true)}, 
+            {"age", Json::string("hehe")}
+        });
+
+    //auto x = { 1, 4.5f, "343"};
+
     return 0;
 }
