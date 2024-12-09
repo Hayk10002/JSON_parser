1baa9d522ad4e95652f3a5148ca70cb6a4122c89
Hayk Khachatryan <hayk.g.khachatryan@gmail.com>
2024-12-09 22:32:04 +0400

    Add test for parse errors

--- tests/parser_error_test.cpp
+++ tests/parser_error_test.cpp
@@ -1,5 +1,16 @@
 #include "parser_error.hpp"
+#include <cassert>
+#include <string>
+
+using hayk10002::json_parser::lexer::Position;
+using hayk10002::json_parser::lexer::UnexpectedCharacter;
+using hayk10002::json_parser::lexer::UnexpectedEndOfInput;
 
 int main() {
+
+    assert(UnexpectedCharacter(Position(15, 4, 3), "a digit").what() == std::string("Unexpected character at line: 4, col: 3  (pos: 15). Expected a digit."));
+    assert(UnexpectedCharacter(Position(15, 4, 3)).what() == std::string("Unexpected character at line: 4, col: 3  (pos: 15)."));
+    assert(UnexpectedEndOfInput(Position(15, 4, 3)).what() == std::string("Unexpected end of input at line: 4, col: 3  (pos: 15)."));
+
     return 0;
 }
